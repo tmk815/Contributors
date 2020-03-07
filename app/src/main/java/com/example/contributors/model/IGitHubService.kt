33ef1abd5de4533432bc6955d11f9e415bd1782d
門田朋己 <monndenn15@gmail.com>
2020-03-07 20:18:34 +0900

    add : GET Contributor list

--- app/src/main/java/com/example/contributors/model/IGitHubService.kt
+++ app/src/main/java/com/example/contributors/model/IGitHubService.kt
@@ -1,4 +1,9 @@
 package com.example.contributors.model
 
+import retrofit2.Response
+import retrofit2.http.GET
+
 interface IGitHubService {
+    @GET("repositories/90792131/contributors")
+    suspend fun getContributorList(): Response<List<Contributors>>
 }
