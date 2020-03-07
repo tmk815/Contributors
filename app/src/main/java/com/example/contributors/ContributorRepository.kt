43b7838be01de17bddd661711b7eb4a57d6c007f
門田朋己 <monndenn15@gmail.com>
2020-03-07 20:26:38 +0900

    add : GitHubAPI URL

---
+++ app/src/main/java/com/example/contributors/ContributorRepository.kt
@@ -0,0 +1,12 @@
+package com.example.contributors
+
+import com.example.contributors.model.IGitHubService.Companion.GITHUB_API_URL
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ContributorRepository {
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(GITHUB_API_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+}
