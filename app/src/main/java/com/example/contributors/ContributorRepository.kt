659787386b5c468f3ec51231e3dc1b565180980a
門田朋己 <monndenn15@gmail.com>
2020-03-07 20:45:49 +0900

    add : IGitHubService create

--- app/src/main/java/com/example/contributors/ContributorRepository.kt
+++ app/src/main/java/com/example/contributors/ContributorRepository.kt
@@ -1,5 +1,6 @@
 package com.example.contributors
 
+import com.example.contributors.model.IGitHubService
 import com.example.contributors.model.IGitHubService.Companion.GITHUB_API_URL
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -9,4 +10,6 @@ class ContributorRepository {
         .baseUrl(GITHUB_API_URL)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
+
+    private var githubService: IGitHubService = retrofit.create(IGitHubService::class.java)
 }
