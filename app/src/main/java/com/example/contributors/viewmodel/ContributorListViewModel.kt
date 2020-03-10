0ee98e4d17fdfc9fa46dc66086c6f10089ea4a3f
門田朋己 <monndenn15@gmail.com>
2020-03-10 12:38:49 +0900

    add : contributorListLiveData

--- app/src/main/java/com/example/contributors/viewmodel/ContributorListViewModel.kt
+++ app/src/main/java/com/example/contributors/viewmodel/ContributorListViewModel.kt
@@ -1,4 +1,33 @@
 package com.example.contributors.viewmodel
 
-class ContributorListViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.contributors.ContributorRepository
+import com.example.contributors.model.Contributor
+import kotlinx.coroutines.launch
+
+class ContributorListViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repository = ContributorRepository.instance
+
+    var contributorListLiveData: MutableLiveData<List<Contributor>> = MutableLiveData()
+
+    init {
+        loadContributorList()
+    }
+
+    private fun loadContributorList() {
+        viewModelScope.launch {
+            try {
+                val request = repository.getContributorList()
+                if (request.isSuccessful) {
+                    contributorListLiveData.postValue(request.body())
+                }
+            } catch (e: Exception) {
+                e.stackTrace
+            }
+        }
+    }
 }
